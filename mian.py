8eaadf2e933c07c9a61c924039d9d2bc9c2ffeff
FlowerHua <1198884646@qq.com>
2023-08-14 11:02:20 +0800

    add some coments

--- mian.py
+++ mian.py
@@ -3,10 +3,12 @@ import time
 
 # url：查看浏览器的开发者工具中的网络一栏，查看某个http请求中的请求网址
 url = "http://gmis.xjtu.edu.cn/pyxx/pygl/pkjlcx/listpk?sEcho=1&iColumns=11&sColumns=&iDisplayStart=0&iDisplayLength=15&mDataProp_0=RN&mDataProp_1=KCBH&mDataProp_2=KCMC&mDataProp_3=BJMC&mDataProp_4=KCXF&mDataProp_5=KCZXS&mDataProp_6=ZJJSMC&mDataProp_7=SKSJ&mDataProp_8=RNRS&mDataProp_9=XKRS&mDataProp_10=KKFS&yxsh=&xq_SELECTNAME=2023%25E7%25A7%258B&xq=49&sftsk=&kcbh=%25E7%25AE%2597%25E6%25B3%2595&_=1691977215985"
-
+    
 # cookie信息也在http请求报文中，直接复制粘贴
-header = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
-            "Cookie":"JSESSIONID=50D430C753AF18DBE5F98C47276DA93D.pyxx_server5.pyxx_server5; _ga=GA1.3.1453050705.1691551101"}
+header = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        "Cookie":"JSESSIONID=EFFB7D2EAE9A2A3B422812D8D2E2B7EB.pyxx_server5.pyxx_server5;\
+                _ga=GA1.3.1453050705.1691551101"}
 
 while True:
     # 查看当前时间
@@ -25,4 +27,6 @@ while True:
     # 查看响应内容，response.text 返回的是Unicode格式的数据
     print("--------返回信息如下--------\n", response.text)
     print("\n")
+    
+    # 等待1s，可以自行删除
     time.sleep(1)
